ba3814d180cc70d5ac6cf00940417ca35d98c299
Tip790 <aartho1@buffs.wtamu.edu>
2026-09-06 17:23:35 -0500

    Display the list on the home page

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+tasks = [
+    {"title": "Finish Homework 1", "completed": False},
+    {"title": "Graduate College", "completed": False},
+    {"title": "Finish Quiz 2", "completed": False},
+    {"title": "Sleep", "completed": True},
+    {"title": "Play Video Games", "completed": True},
+]
+
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "home.html", {"tasks": tasks})
 def about(request):
     return render(request, "about.html")
 def contact(request):
